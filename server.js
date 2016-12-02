ec09f41f6c2dcd7327e168d46f3ce1ca93e03caa
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-12-02 01:40:51 -0800

    simple about route made to check server is made

---
+++ server.js
@@ -0,0 +1,18 @@
+var express=require('express');
+var app=express();
+var PORT=process.env.PORT || 3000;
+var id=1;
+var users=[];
+var body_parser=require('body-parser');
+var db=require('./db.js');
+var _=require('underscore');
+var bcrypt=require('bcrypt');
+
+//about route
+app.get("/about",function(req,res){
+res.send("made on request of Naeem Siddiq");
+});
+
+app.listen(PORT,function(){
+	console.log("Listening on port"+PORT);
+});
